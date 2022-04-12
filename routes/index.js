9341ccde579a58a0c6871879bbae2075b1683d4f
Hoat123 <hoathmph15543@fpt.edu.vn>
2022-04-12 23:39:32 +0700

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,95 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.get('/Them',function (req , res,next ){
+  Anh.find({},function (err,data){
+    res.render('Them',{title : 'Them', data : data});
+  })
+});
+var dbb = "mongodb+srv://hoathmph15543:haminhhoat@cluster0.zmyqv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const mongoose = require('mongoose');
+mongoose.connect(dbb).catch(error => {
+  console.log("lỗi xảy ra " + error)
+})
+var AnhSchema = new mongoose.Schema({
+  Anh:'string' ,
+  noiDung : 'string',
+  ngayThang : 'string',
+  linkAnh : 'string'
+});
+var Anh = mongoose.model('anhne' , AnhSchema);
+router.post('/ThemAnh' , function (req , res){
+
+  var anh = req.body.Anh;
+  var noiDung = req.body.noiDung;
+  var ngayThang = req.body.ngayThang;
+  var linkAnh = req.body.linkAnh;
+
+
+  const anhne = new Anh({
+    Anh : anh,
+    noiDung: noiDung,
+    ngayThang: ngayThang,
+    linkAnh: linkAnh
+  });
+  anhne.save(function (error){
+    var mess;
+    if(error == null){
+      mess = "Thêm thành công"
+    }else {
+      mess = error
+    }
+  })
+
+  console.log(anh + noiDung + ngayThang + linkAnh);
+
+  res.render('Them');
+});
+router.get('/DanhSach' , function (req , res, next){
+  Anh.find({},function (err , data){
+    res.render('DanhSach',{data : data});
+  });
+});
+router.get('/DanhSachAll' , function (req , res, next){
+  Anh.find({},function (err , data){
+    res.send( data );
+  });
+});
+router.post('/Delete',function (req , res ){
+  let ObejectID = require('mongodb').ObjectId;
+  var id = req.body.id;
+  Anh.deleteOne({_id :  ObejectID(id)}, function (err){
+    if(err){
+      console.log("lỗi ");
+    }else {
+      console.log("Xóa thành công")
+    }
+  })
+})
+router.get('/Update', function (req , res, next){
+  Anh.find({},function (err, data){
+    res.render('Update',{title : "Update" , data : data});
+  });
+
+
+
+});
+router.post('/Update' , function (req , res ){
+  let ObejectID = require('mongodb').ObjectId;
+  var id = req.body.id;
+  var anh = req.body.Anh;
+  var noiDung = req.body.noiDung;
+  var ngayThang = req.body.ngayThang;
+  var linkAnh = req.body.linkAnh;
+
+  Anh.updateOne({_id : ObejectID(id)}, {Anh: anh, noiDung : noiDung, ngayThang: ngayThang , linkAnh : linkAnh}, function (err){
+    if(err) throw err;
+    console.log('Sua thanh cong');
+  })
+})
+
+module.exports = router;
